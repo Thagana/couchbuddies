b9423a253b7dd17247ff43c9b885592a8f873cab
Thagana <samuelmthwa79@gmail.com>
2020-06-16 23:51:32 +0200

    :rocket: login Screen

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,11 @@
+// @flow
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function LoginScreen() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
