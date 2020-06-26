9c10ef7819a946229776c1322fc87c747290bcb5
Thagana <samuelmthwa79@gmail.com>
2020-06-26 18:39:54 +0200

    :rocket: Root component

--- src/Root.js
+++ src/Root.js
@@ -2,17 +2,17 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {createSharedElementStackNavigator} from 'react-navigation-shared-element';
 
 import HomeScreen from './screens/HomeScreen';
 import MovieListScreen from './screens/MovieListScreen';
-import ProfileScreen from './screens/ProfileScreen';
+import MovieScreen from './screens/Movie';
 
-const HomeStack = createStackNavigator();
+const HomeStack = createSharedElementStackNavigator();
 const HomeStackScreen = () => {
   return (
-    <HomeStack.Navigator>
+    <HomeStack.Navigator headerMode="none">
       <HomeStack.Screen
         name="CouchBuddies"
         options={{
@@ -26,25 +26,19 @@ const HomeStackScreen = () => {
         }}
         component={HomeScreen}
       />
+      <HomeStack.Screen
+        name="Movie"
+        component={MovieScreen}
+        sharedElementsConfig={(route, otherRoute, showing) => {
+          const {item} = route.params;
+          return [
+            `item.${item.id}.id`,
+            `item.${item.poster}.poster`,
+            `item.${item.overview}.overview`,
+          ];
+        }}
+      />
     </HomeStack.Navigator>
-  );
-};
-
-const MovieStack = createStackNavigator();
-const MovieStackScreen = () => {
-  return (
-    <MovieStack.Navigator>
-      <MovieStack.Screen name="Movie List" component={MovieListScreen} />
-    </MovieStack.Navigator>
-  );
-};
-
-const ProfileStack = createStackNavigator();
-const ProfileStackScreen = () => {
-  return (
-    <ProfileStack.Navigator>
-      <ProfileStack.Screen name="Profile" component={ProfileScreen} />
-    </ProfileStack.Navigator>
   );
 };
 
@@ -69,7 +63,7 @@ export default function Root() {
         />
         <TabNavigator.Screen
           name="Movie List"
-          component={MovieStackScreen}
+          component={MovieListScreen}
           options={{
             tabBarIcon: ({color, size}) => (
               <MaterialCommunityIcons
